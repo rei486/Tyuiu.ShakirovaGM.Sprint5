4db1efca82495174279ba9d2c2907bccf768ca34
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-30 01:20:22 +0500

    Task2

---
+++ Tyuiu.ShakirovaGM.Sprint5.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.ShakirovaGM.Sprint5.Task2.V5.Lib;
+using System.IO;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint5.Task2.V5/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.ShakirovaGM.Sprint5.Task2.V5.Lib;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task2.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шакирова Г. М. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Шакирова Гузель Мансуровна | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция,(произвести табулирование) f(x) на заданном диапазоне      *");
+            Console.WriteLine("* [-5; 5] с шагом 1. Произвести проверку деления на ноль. При делении на  *");
+            Console.WriteLine("* ноль вернуть значение 0. результат сохранить в текстовый файл           *");
+            Console.WriteLine("* OutPutFileTask1.txt и вывести на консоль в таблицу.                     *");
+            Console.WriteLine("* Значения округлить до двух знаков после запятой.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] mtrx = new int[3, 3] { { 9, 6, 6},
+                                          { 8, 7, 2},
+                                          { 1, 7, 8}};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int cols = mtrx.Length / rows;
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadLine();
+        }
+    }
+}
