5951e86c17dc8f6af8ec394e20df0786d887899f
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-30 02:40:06 +0500

    Task4

---
+++ Tyuiu.ShakirovaGM.Sprint5.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task4.V3.Lib
+{
+    public class DataService : ISprint5Task4V3
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX=File.ReadAllText(path);
+            double res = Math.Round(Math.Pow((Convert.ToDouble(strX) - 1), (3 * Convert.ToDouble(strX) + 1)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint5.Task4.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ShakirovaGM.Sprint5.Task4.V3.Lib;
+using System.IO;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task4.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шакирова Г. М. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Шакирова Гузель Мансуровна | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt (файл взять из архива*");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:\\DataSprint5\\ и    *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть вещественное значение. Прочитать *");
+            Console.WriteLine("* значение из файла и подставить вместо Х в формуле . Вычислить значение  *");
+            Console.WriteLine("* по формуле (Полученное значение округлить до трёх знаков после запятой) *");
+            Console.WriteLine("* и вернуть полученный результат на консоль.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"С:\DataSprint5\InPutDataFileTask4V3.txt";
+            Console.WriteLine(" Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+           
+            Console.ReadKey();
+        }
+    }
+}
