a904a8b952e7e2c35ee3a3e6acae2db47c8ae90c
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-30 01:52:33 +0500

    Task3

---
+++ Tyuiu.ShakirovaGM.Sprint5.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task3.V27.Lib
+{
+    public class DataService : ISprint5Task3V27
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double y = Math.Round(Math.Pow((x - 1), 3 * x + 1), 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint5.Task3.V27/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ShakirovaGM.Sprint5.Task3.V27.Lib;
+namespace Tyuiu.ShakirovaGM.Sprint5.Task3.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шакирова Г. М. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Шакирова Гузель Мансуровна | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до    *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadLine();
+        }
+    }
+}
