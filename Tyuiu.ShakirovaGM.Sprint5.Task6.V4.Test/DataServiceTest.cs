bd192ef32a506539c62f6f9a0408915a0a166069
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-12-28 07:27:31 +0500

    Task6

---
+++ Tyuiu.ShakirovaGM.Sprint5.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.ShakirovaGM.Sprint5.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
